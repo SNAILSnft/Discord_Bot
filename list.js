6773a1c13b9b388444f39fffe7866a9885626498
the logical guy <146027539+Something-important@users.noreply.github.com>
2024-01-30 21:56:09 +0530

    adding necessary files

---
+++ list.js
@@ -0,0 +1,15 @@
+//  word search on discord
+module.exports = [
+    // Cosmos Ecosystem Staking Platforms
+    { name: "where do i stake", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
+    { name: "staking platforms", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
+    { name: "can i stake", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
+    { name: "$stake", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
+    // Leap Snaps on MetaMask
+    { name: "can i use metamask", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
+    { name: "metamask", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
+    { name: "instead of keplr", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
+    // Squid Router
+    { name: "what bridge", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
+    { name: "transfer from", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
+];
