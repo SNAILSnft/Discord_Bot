12611f9b49740cf3eb55228d998391b76fb8b455
SNAILS <153635978+SNAILSnft@users.noreply.github.com>
2024-02-22 12:32:44 -0500

    Update list.js

--- list.js
+++ list.js
@@ -8,30 +8,30 @@ module.exports = [
     { name: "can i use metamask", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
     { name: "$leapsnaps", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
     // Squid Router
-    { name: "how do I bridge", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
+    { name: "how do i bridge", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
     { name: "$squidrouter", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
     // Claiming Creds
-    { name: "how do I claim my creds", URL: "https://omniflix.tv/iv/65c3cbc0c65a434875481c75" },
+    { name: "how do i claim my creds", URL: "https://omniflix.tv/iv/65c3cbc0c65a434875481c75" },
     { name: "$claimcreds", URL: "https://omniflix.tv/iv/65c3cbc0c65a434875481c75" },
     // Secret Network Overview
     { name: "$secretnetwork", URL: "https://omniflix.tv/iv/65b65696c65a434875168589" },
     // Uploading files
-    { name: "how do I upload", URL: "https://omniflix.tv/iv/65affbbeb79d23e989a67385" },
+    { name: "how do i upload", URL: "https://omniflix.tv/iv/65affbbeb79d23e989a67385" },
     { name: "$jackalstorage", URL: "https://omniflix.tv/iv/65affbbeb79d23e989a67385" },
     // Sticker Packs
     { name: "$telegramstickers", URL: "https://omniflix.tv/iv/65860b86d661a858ffdc8e96" },  
     // Shade Protocol
-    { name: "what is Shade", URL: "https://omniflix.tv/iv/65a7d92a59f3c03dc394e3be" },
+    { name: "what is shade", URL: "https://omniflix.tv/iv/65a7d92a59f3c03dc394e3be" },
     { name: "$shade", URL: "https://omniflix.tv/iv/65a7d92a59f3c03dc394e3be" },    
     // DVPN Meile
     { name: "what vpn", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },
     { name: "$meile", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },  
      // Stargaze Standard Collection
-    { name: "how do I mint on Stargaze", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },
+    { name: "how do i mint on stargaze", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },
     { name: "$stargazemint", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },  
      // Entering the Cosmos
     { name: "$enteringcosmos", URL: "https://medium.com/@snailsnft/entering-the-cosmos-ecosystem-a70f1c5bf4a3" },    
      // Validator Selection
-    { name: "who do I stake with", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },
+    { name: "who do i stake with", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },
     { name: "$validatorselection", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },  
 ];
