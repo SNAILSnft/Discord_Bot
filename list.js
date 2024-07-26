a441a9e2c2ab611e4146ce6c0e08b1d006d80dec
the logical guy <146027539+Something-important@users.noreply.github.com>
2024-07-27 00:06:45 +0530

    updated the list

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-//  word search on discord
+// Word search on Discord
 module.exports = [
     // Cosmos Ecosystem Staking Platforms
     { name: "where do i stake", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
@@ -27,12 +27,12 @@ module.exports = [
     { name: "what vpn", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },
     { name: "$meile", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },  
     { name: "$dvpn", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },  
-     // Stargaze Standard Collection
+    // Stargaze Standard Collection
     { name: "how do i mint on stargaze", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },
     { name: "$stargazemint", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },  
-     // Entering the Cosmos
+    // Entering the Cosmos
     { name: "$enteringcosmos", URL: "https://medium.com/@snailsnft/entering-the-cosmos-ecosystem-a70f1c5bf4a3" },    
-     // Validator Selection
+    // Validator Selection
     { name: "who do i stake with", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },
     { name: "$validatorselection", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },  
     // Astrovault
@@ -45,12 +45,41 @@ module.exports = [
     { name: "who is dail", URL: "https://medium.com/@snailsnft/dail-discord-bot-0ba084ca0015" },
     { name: "$dail", URL: "https://medium.com/@snailsnft/dail-discord-bot-0ba084ca0015" },  
     // The Future of the ATOM Economic Zone
-    { name: "what the future for atom", URL: "https://medium.com/@snailsnft/dail-discord-bot-0ba084ca0015" },
+    { name: "what the future for atom", URL: "https://medium.com/@snailsnft/the-future-of-the-atom-economic-zone-b3396347fb56" },
     { name: "$aez", URL: "https://medium.com/@snailsnft/the-future-of-the-atom-economic-zone-b3396347fb56" }, 
+    // Stake ATOM using Cosmostation
+    { name: "cosmostation", URL: "https://medium.com/@snailsnft/stake-atom-using-cosmostation-c66276aaa13a" },
+    { name: "$cosmostation", URL: "https://medium.com/@snailsnft/stake-atom-using-cosmostation-c66276aaa13a" }, 
+    // Stake ATOM using Keplr
+    { name: "stake with keplr", URL: "https://medium.com/@snailsnft/stake-atom-using-keplr-f13f277089b4" },
+    { name: "$keplrstake", URL: "https://medium.com/@snailsnft/stake-atom-using-keplr-f13f277089b4" }, 
+    // FAQ - Node & Validator
+    { name: "how do i setup a node", URL: "https://medium.com/@snailsnft/faq-node-validator-73cf5380b1ad" },
+    { name: "$validator", URL: "https://medium.com/@snailsnft/faq-node-validator-73cf5380b1ad" }, 
+    // FAQ - Sentry Node Array Tutorial
+    { name: "$sentrynode", URL: "https://medium.com/@snailsnft/sentry-node-array-tutorial-c9401c3d3ff3" }, 
+    // FAQ - Exploring Neutron's Ecosystem
+    { name: "neutron apps", URL: "https://x.com/SNAILSNFT_/status/1796331854011703344" }, 
+    { name: "$neutron", URL: "https://x.com/SNAILSNFT_/status/1796331854011703344" }, 
+    // FAQ - What is Atlas DAO
+    { name: "nft lending", URL: "https://omniflix.tv/iv/663b8f743a6fdeaf2805f210" }, 
+    { name: "$atlas", URL: "https://omniflix.tv/iv/663b8f743a6fdeaf2805f210" }, 
+    // FAQ - Isaac Trading Bot by Osmocake
+    { name: "isaac trading bot", URL: "https://omniflix.tv/iv/664c989dc3f0c7c68971da42" }, 
+    { name: "$osmoisaac", URL: "https://omniflix.tv/iv/664c989dc3f0c7c68971da42" }, 
+    // FAQ - Joining ohhNFT Raffles
+    { name: "how do i join ohhnft", URL: "https://omniflix.tv/iv/668565c8349e1261a1e5e68a" }, 
+    { name: "$ohhnft", URL: "https://omniflix.tv/iv/668565c8349e1261a1e5e68a" },
+    // FAQ - Staking on ohhNFT
+    { name: "how do i stake on ohhnft", URL: "https://omniflix.tv/iv/668575b9422582fb417f469f" }, 
+    { name: "$ohhnftstaking", URL: "https://omniflix.tv/iv/668575b9422582fb417f469f" },
+    // FAQ - Catyclops Migration
+    { name: "migrate cats", URL: "https://omniflix.tv/iv/66a10e3b1d672071fd546014" }, 
+    { name: "$catyclops", URL: "https://omniflix.tv/iv/66a10e3b1d672071fd546014" },
     // Medium
     { name: "$medium", URL: "https://medium.com/@snailsnft/" }, 
     // OmniFlix
-    { name: "$omniflix", URL: " https://omniflix.tv/channel/65182782e1c28773aa199c84" }, 
+    { name: "$omniflix", URL: "https://omniflix.tv/channel/65182782e1c28773aa199c84" }, 
     // YouTube
     { name: "$youtube", URL: "https://www.youtube.com/@SNAILS._/videos" },
 ];
