d7413aab846bd2a696130d1df5baf7e858d1dd69
the logical guy <146027539+Something-important@users.noreply.github.com>
2024-08-29 23:08:06 +0530

    Update list.js

--- list.js
+++ list.js
@@ -1,85 +1,55 @@
-// Word search on Discord
 module.exports = [
-    // Cosmos Ecosystem Staking Platforms
     { name: "where do i stake", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
     { name: "staking platforms", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
     { name: "$staking", URL: "https://omniflix.tv/iv/657ccb6bd661a858ffc70aed" },
-    // Leap Snaps on MetaMask
     { name: "can i use metamask", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
     { name: "$leapsnaps", URL: "https://omniflix.tv/iv/657ccf58d661a858ffc71434" },
-    // Squid Router
     { name: "how do i bridge", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
     { name: "$squidrouter", URL: "https://omniflix.tv/iv/657ccf08d661a858ffc712bb" },
-    // Claiming Creds
     { name: "how do i claim my creds", URL: "https://omniflix.tv/iv/65c3cbc0c65a434875481c75" },
     { name: "$claimcreds", URL: "https://omniflix.tv/iv/65c3cbc0c65a434875481c75" },
-    // Secret Network Overview
     { name: "$secretnetwork", URL: "https://omniflix.tv/iv/65b65696c65a434875168589" },
-    // Uploading files
     { name: "how do i upload", URL: "https://omniflix.tv/iv/65affbbeb79d23e989a67385" },
     { name: "$jackalstorage", URL: "https://omniflix.tv/iv/65affbbeb79d23e989a67385" },
-    // Sticker Packs
-    { name: "$telegramstickers", URL: "https://omniflix.tv/iv/65860b86d661a858ffdc8e96" },  
-    // Shade Protocol
+    { name: "$telegramstickers", URL: "https://omniflix.tv/iv/65860b86d661a858ffdc8e96" },
     { name: "what is shade", URL: "https://omniflix.tv/iv/65a7d92a59f3c03dc394e3be" },
-    { name: "$shade", URL: "https://omniflix.tv/iv/65a7d92a59f3c03dc394e3be" },    
-    // DVPN Meile
+    { name: "$shade", URL: "https://omniflix.tv/iv/65a7d92a59f3c03dc394e3be" },
     { name: "what vpn", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },
-    { name: "$meile", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },  
-    { name: "$dvpn", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },  
-    // Stargaze Standard Collection
+    { name: "$meile", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },
+    { name: "$dvpn", URL: "https://omniflix.tv/iv/657cd383d661a858ffc71dec" },
     { name: "how do i mint on stargaze", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },
-    { name: "$stargazemint", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },  
-    // Entering the Cosmos
-    { name: "$enteringcosmos", URL: "https://medium.com/@snailsnft/entering-the-cosmos-ecosystem-a70f1c5bf4a3" },    
-    // Validator Selection
+    { name: "$stargazemint", URL: "https://medium.com/@snailsnft/stargaze-creating-a-standard-collection-c2acf6076f67" },
+    { name: "$enteringcosmos", URL: "https://medium.com/@snailsnft/entering-the-cosmos-ecosystem-a70f1c5bf4a3" },
     { name: "who do i stake with", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },
-    { name: "$validatorselection", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },  
-    // Astrovault
+    { name: "$validatorselection", URL: "https://omniflix.tv/iv/65ce2e4ec65a434875669e3d" },
     { name: "what is astrovault", URL: "https://omniflix.tv/iv/6604486447ea52133bf72322" },
-    { name: "$astrovault", URL: "https://omniflix.tv/iv/6604486447ea52133bf72322" },  
-    // Amber
+    { name: "$astrovault", URL: "https://omniflix.tv/iv/6604486447ea52133bf72322" },
     { name: "what is amber", URL: "https://omniflix.tv/iv/661479365e962db5dad46b4f" },
-    { name: "$amber", URL: "https://omniflix.tv/iv/661479365e962db5dad46b4f" },  
-    // Dail
+    { name: "$amber", URL: "https://omniflix.tv/iv/661479365e962db5dad46b4f" },
     { name: "who is dail", URL: "https://medium.com/@snailsnft/dail-discord-bot-0ba084ca0015" },
-    { name: "$dail", URL: "https://medium.com/@snailsnft/dail-discord-bot-0ba084ca0015" },  
-    // The Future of the ATOM Economic Zone
+    { name: "$dail", URL: "https://medium.com/@snailsnft/dail-discord-bot-0ba084ca0015" },
     { name: "what the future for atom", URL: "https://medium.com/@snailsnft/the-future-of-the-atom-economic-zone-b3396347fb56" },
-    { name: "$aez", URL: "https://medium.com/@snailsnft/the-future-of-the-atom-economic-zone-b3396347fb56" }, 
-    // Stake ATOM using Cosmostation
+    { name: "$aez", URL: "https://medium.com/@snailsnft/the-future-of-the-atom-economic-zone-b3396347fb56" },
     { name: "cosmostation", URL: "https://medium.com/@snailsnft/stake-atom-using-cosmostation-c66276aaa13a" },
-    { name: "$cosmostation", URL: "https://medium.com/@snailsnft/stake-atom-using-cosmostation-c66276aaa13a" }, 
-    // Stake ATOM using Keplr
+    { name: "$cosmostation", URL: "https://medium.com/@snailsnft/stake-atom-using-cosmostation-c66276aaa13a" },
     { name: "stake with keplr", URL: "https://medium.com/@snailsnft/stake-atom-using-keplr-f13f277089b4" },
-    { name: "$keplrstake", URL: "https://medium.com/@snailsnft/stake-atom-using-keplr-f13f277089b4" }, 
-    // FAQ - Node & Validator
+    { name: "$keplrstake", URL: "https://medium.com/@snailsnft/stake-atom-using-keplr-f13f277089b4" },
     { name: "how do i setup a node", URL: "https://medium.com/@snailsnft/faq-node-validator-73cf5380b1ad" },
-    { name: "$validator", URL: "https://medium.com/@snailsnft/faq-node-validator-73cf5380b1ad" }, 
-    // FAQ - Sentry Node Array Tutorial
-    { name: "$sentrynode", URL: "https://medium.com/@snailsnft/sentry-node-array-tutorial-c9401c3d3ff3" }, 
-    // FAQ - Exploring Neutron's Ecosystem
-    { name: "neutron apps", URL: "https://x.com/SNAILSNFT_/status/1796331854011703344" }, 
-    { name: "$neutron", URL: "https://x.com/SNAILSNFT_/status/1796331854011703344" }, 
-    // FAQ - What is Atlas DAO
-    { name: "nft lending", URL: "https://omniflix.tv/iv/663b8f743a6fdeaf2805f210" }, 
-    { name: "$atlas", URL: "https://omniflix.tv/iv/663b8f743a6fdeaf2805f210" }, 
-    // FAQ - Isaac Trading Bot by Osmocake
-    { name: "isaac trading bot", URL: "https://omniflix.tv/iv/664c989dc3f0c7c68971da42" }, 
-    { name: "$osmoisaac", URL: "https://omniflix.tv/iv/664c989dc3f0c7c68971da42" }, 
-    // FAQ - Joining ohhNFT Raffles
-    { name: "how do i join ohhnft", URL: "https://omniflix.tv/iv/668565c8349e1261a1e5e68a" }, 
+    { name: "$validator", URL: "https://medium.com/@snailsnft/faq-node-validator-73cf5380b1ad" },
+    { name: "$sentrynode", URL: "https://medium.com/@snailsnft/sentry-node-array-tutorial-c9401c3d3ff3" },
+    { name: "neutron apps", URL: "https://x.com/SNAILSNFT_/status/1796331854011703344" },
+    { name: "$neutron", URL: "https://x.com/SNAILSNFT_/status/1796331854011703344" },
+    { name: "nft lending", URL: "https://omniflix.tv/iv/663b8f743a6fdeaf2805f210" },
+    { name: "$atlas", URL: "https://omniflix.tv/iv/663b8f743a6fdeaf2805f210" },
+    { name: "isaac trading bot", URL: "https://omniflix.tv/iv/664c989dc3f0c7c68971da42" },
+    { name: "$osmoisaac", URL: "https://omniflix.tv/iv/664c989dc3f0c7c68971da42" },
+    { name: "how do i join ohhnft", URL: "https://omniflix.tv/iv/668565c8349e1261a1e5e68a" },
     { name: "$ohhnft", URL: "https://omniflix.tv/iv/668565c8349e1261a1e5e68a" },
-    // FAQ - Staking on ohhNFT
-    { name: "how do i stake on ohhnft", URL: "https://omniflix.tv/iv/668575b9422582fb417f469f" }, 
+    { name: "how do i stake on ohhnft", URL: "https://omniflix.tv/iv/668575b9422582fb417f469f" },
     { name: "$ohhnftstaking", URL: "https://omniflix.tv/iv/668575b9422582fb417f469f" },
-    // FAQ - Catyclops Migration
-    { name: "migrate cats", URL: "https://omniflix.tv/iv/66a10e3b1d672071fd546014" }, 
+    { name: "migrate cats", URL: "https://omniflix.tv/iv/66a10e3b1d672071fd546014" },
     { name: "$catyclops", URL: "https://omniflix.tv/iv/66a10e3b1d672071fd546014" },
-    // Medium
-    { name: "$medium", URL: "https://medium.com/@snailsnft/" }, 
-    // OmniFlix
-    { name: "$omniflix", URL: "https://omniflix.tv/channel/65182782e1c28773aa199c84" }, 
-    // YouTube
+    { name: "$medium", URL: "https://medium.com/@snailsnft/" },
+    { name: "$omniflix", URL: "https://omniflix.tv/channel/65182782e1c28773aa199c84" },
     { name: "$youtube", URL: "https://www.youtube.com/@SNAILS._/videos" },
 ];
